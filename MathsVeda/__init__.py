3c5113dff8d56337be2e33a99cd70317150f8e71
Ankit <135586622+ankitgupta-23@users.noreply.github.com>
2023-07-07 11:02:45 +0530

    Update and rename app.py to __init__.py

---
+++ MathsVeda/__init__.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+
+def create_app():
+
+    app = Flask(__name__)
+
+    from .main import main as main_blueprint
+    from .handle_exceptions import handle_exceptions as h_e_blueprint
+    app.register_blueprint(main_blueprint)
+    app.register_blueprint(h_e_blueprint)
+
+    return app
