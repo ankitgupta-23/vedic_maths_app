971340d941275145baca14e258f632406f31f0b4
ankit <ankitkumargupta9968@gmail.com>
2023-07-08 17:18:47 +0530

    updated email credentials

--- MathsVeda/send_contact_data.py
+++ MathsVeda/send_contact_data.py
@@ -1,8 +1,7 @@
 
 from flask_mail import Mail, Message
 from flask import render_template
-
-
+import os
 
 
 
@@ -10,16 +9,21 @@ def send_message(data):
     from app import app
     app.config['MAIL_SERVER']='smtp.gmail.com'
     app.config['MAIL_PORT'] = 465
-    app.config['MAIL_USERNAME'] = 'contact.mathsveda.mail@gmail.com'
-    app.config['MAIL_PASSWORD'] = 'rypgvbfhglwucaik'
+    app.config['MAIL_USERNAME'] = os.environ.get('USER_NAME')
+    app.config['MAIL_PASSWORD'] = os.environ.get('PASSWORD')
     app.config['MAIL_USE_TLS'] = False
     app.config['MAIL_USE_SSL'] = True
+
+    sender = os.environ.get('USER_NAME')
+    reciever1 = os.environ.get('REC_MAIL_1')
+    reciever2 = os.environ.get('REC_MAIL_2')
+
     mail = Mail(app)
 
     html_message  = render_template('email_message.html', name = data["name"], email = data["email"], message = data["message"])
 
     
-    msg = Message(subject = "Mail from Mathsveda", sender = 'contact.mathsveda.mail@gmail.com', recipients = ['ankitgupta.imp21@gmail.com', 'query.mathsveda@gmail.com'])
+    msg = Message(subject = "Mail from Mathsveda", sender = sender, recipients = [reciever1, reciever2])
     msg.html = html_message
     mail.send(msg)
     print("sent successfully !")
