86994b6e08801ddca80e49510409afa487b9242c
ankit <ankitkumargupta9968@gmail.com>
2023-07-08 17:12:27 +0530

    contact form backend added for sending email

--- MathsVeda/send_contact_data.py
+++ MathsVeda/send_contact_data.py
@@ -1,6 +1,6 @@
 
 from flask_mail import Mail, Message
-
+from flask import render_template
 
 
 
@@ -10,9 +10,16 @@ def send_message(data):
     from app import app
     app.config['MAIL_SERVER']='smtp.gmail.com'
     app.config['MAIL_PORT'] = 465
-    app.config['MAIL_USERNAME'] = 'ankitgupta.imp21@gmail.com'
-    app.config['MAIL_PASSWORD'] = '9315689723'
+    app.config['MAIL_USERNAME'] = 'contact.mathsveda.mail@gmail.com'
+    app.config['MAIL_PASSWORD'] = 'rypgvbfhglwucaik'
     app.config['MAIL_USE_TLS'] = False
     app.config['MAIL_USE_SSL'] = True
     mail = Mail(app)
+
+    html_message  = render_template('email_message.html', name = data["name"], email = data["email"], message = data["message"])
+
     
+    msg = Message(subject = "Mail from Mathsveda", sender = 'contact.mathsveda.mail@gmail.com', recipients = ['ankitgupta.imp21@gmail.com', 'query.mathsveda@gmail.com'])
+    msg.html = html_message
+    mail.send(msg)
+    print("sent successfully !")
